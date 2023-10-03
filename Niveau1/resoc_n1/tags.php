ac540fc592a2298c4b5dcb382f2de3c2b7dc9a29
kailam-ada <kai_lam@hotmail.fr>
2023-10-03 15:46:06 +0200

    tags.php

--- Niveau1/resoc_n1/tags.php
+++ Niveau1/resoc_n1/tags.php
@@ -37,13 +37,13 @@
                 $lesInformations = $mysqli->query($laQuestionEnSql);
                 $tag = $lesInformations->fetch_assoc();
                 //@todo: afficher le résultat de la ligne ci dessous, remplacer XXX par le label et effacer la ligne ci-dessous
-                echo "<pre>" . print_r($tag, 1) . "</pre>";
+                // echo "<pre>" . print_r($tag, 1) . "</pre>";
                 ?>
                 <img src="user.jpg" alt="Portrait de l'utilisatrice"/>
                 <section>
                     <h3>Présentation</h3>
                     <p>Sur cette page vous trouverez les derniers messages comportant
-                        le mot-clé XXX
+                        le mot-clé #<?php echo $tag['label'] ?>
                         (n° <?php echo $tagId ?>)
                     </p>
 
@@ -82,24 +82,26 @@
                 while ($post = $lesInformations->fetch_assoc())
                 {
 
-                    echo "<pre>" . print_r($post, 1) . "</pre>";
+                    // echo "<pre>" . print_r($post, 1) . "</pre>";
                     ?>                
                     <article>
                         <h3>
-                            <time datetime='2020-02-01 11:12:13' >31 février 2010 à 11h12</time>
+                            <time><?php echo $post['created'] ?></time>
                         </h3>
-                        <address>par AreTirer</address>
+                        <address><?php echo "par " . $post['author_name'] ?></address>
                         <div>
-                            <p>Ceci est un paragraphe</p>
-                            <p>Ceci est un autre paragraphe</p>
-                            <p>... de toutes manières il faut supprimer cet 
-                                article et le remplacer par des informations en 
-                                provenance de la base de donnée</p>
+                            <p><?php echo $post['content']   ?></p>
                         </div>                                            
                         <footer>
-                            <small>♥ 132</small>
-                            <a href="">#lorem</a>,
-                            <a href="">#piscitur</a>,
+                            <small>♥ <?php echo $post['like_number']   ?></small>
+                            <a href="">
+                                <?php $tags = $post['taglist'];
+                                    $tagArray = explode(',', $tags);
+                                    foreach ($tagArray as $tags) {
+                                        echo '<a href="#"> #' . ($tags) . '</a>';
+                                    }
+                                ?>
+                            </a>
                         </footer>
                     </article>
                 <?php } ?>
